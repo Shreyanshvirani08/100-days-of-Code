a489282b832e8c77a9607315b74e37cb63a93a79
Shreyanshvirani08 <virani.shreyansh799@gmail.com>
2026-08-13 19:39:02 +0530

    Add files via upload

---
+++ day12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter the two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("sum = %d\n", a + b);
+    printf("difference = %d\n", a - b);
+    printf("product = %d\n", a * b);
+    printf("quotient = %d\n", a / b);
+
+    return 0;
+}
